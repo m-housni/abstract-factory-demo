e18428e2ebc1939a3f995b8f8849cd101490fbf0
m-housni <mohamedhousni1000k@gmail.com>
2025-03-04 10:43:22 +0000

    feat: add homepage and deploy script to package.json; update svelte.config.js for base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+export default defineConfig({
+  base: '/abstract-factory-demo/', // Replace <REPO_NAME> with your repository name
+  plugins: [svelte()],
+})
